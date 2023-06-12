afb4ba6836dbd2016cec00e203639ae84aec2d85
Aditi <Aditi.jangra2005@gmail.com>
2023-06-12 07:08:05 +0530

    Updated Add and Sub functions added

---
+++ main_v3.py
@@ -0,0 +1,65 @@
+def add():
+    while True:
+        try:
+            x = int(input('\nEnter first number: '))
+            y = int(input('Enter second number: '))
+            break   
+        except:
+            print('Please enter an integer value!!!')
+    z = x + y
+
+    print("Sum of ",x,'and ', y,'is =',z)
+
+def sub():
+    while True:
+        try:
+            x = int(input('\nEnter first number: '))
+            y = int(input('Enter second number: '))
+            break   
+        except:
+            print('Please enter an integer value!!!')
+    z = x - y
+
+    print("Difference of ",x,'and ', y,'is =',z)
+
+def mul():
+    x = int(input('\nEnter first number: '))
+    y = int(input('Enter second number: '))
+    z = x * y
+
+    print("product of ",x,'and ', y,'is =',z)
+
+def div():
+    x = int(input('\nEnter first number: '))
+    y = int(input('Enter second number: '))
+    z = x / y
+
+    print("division of ",x,'and ', y,'is =',z)
+
+
+while True:
+    print("________________________\n")
+    print("WELLCOME TO CALCULATOR!!")
+
+    ch = input('''Select from the following options:
+        1.Addition 
+        2.Subtraction
+        3.Multiplication
+        4.Division
+        5.exit
+        ''')
+    
+    if ch == '1':
+        add()
+    elif ch == '2':
+        sub()
+    elif ch == '3':
+        mul()
+    elif ch == '4':
+        div()
+    elif ch == '5':
+        break
+    else:
+        print("Select a valid option")
+
+print("Thank you for using the program....")
